5f0d420a499aa7e95d6c9341cdd3b8e7d6d6220b
Matt Gifford <mattgif@gmail.com>
2017-12-07 16:38:16 -0500

    added handlebars templating engine; moved html to html file

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// propublica congress API key
 const PROPUBLICA_API_KEY = 'eKsVN99hpB4wdCIFiDwTUxd1QRA45ACta0PWdxJo'
 const PROPUBLICA_ENDPOINT = 'https://api.propublica.org/congress/v1/bills/search.json'
-// opensecrets API key
-const OPENSECRETS_API_KEY = '4f65ef721d8386530390255880acbbb9'
-
+const MAPLIGHT_API_KEY = '01f384a9c18616b752063d174eda5fee'
 const settings = {
 	// settings for how data gets displayed
 	titleLength: 125,
@@ -28,14 +25,22 @@ function getDataFromPropublica(searchTerm,callback){
 function renderBillResults(item){
 	// returns html used to display bill results
 	const title = renderTitle(item);	
-	return `
-		<div class="results-bill-tile">
-			<h4 class='results-bill-title'><a href="${item.bill_uri}" class="bill-request">${title}</a></h4>
-			<p>Bill number: ${item.number} | Introduced ${item.introduced_date}</p>
-			<p>Subject: ${item.primary_subject}</p>
-			<p>Sponsor: <a href="${item.sponsor_uri}" class="rep-request">${item.sponsor_title} ${item.sponsor_name} (<span class="affil-${item.sponsor_party}">${item.sponsor_party}</span>-${item.sponsor_state})</a></p>
-		</div>
-	`
+	const context = {
+		bill_uri: item.bill_uri, 
+		title: title, 
+		number: item.number, 
+		introduced_date: item.introduced_date, 
+		subject: item.primary_subject,
+		sponsor_uri: item.sponsor_uri,
+		sponsor: item.sponsor_name,
+		sponsor_title: item.sponsor_title,
+		sponsor_party: item.sponsor_party,
+		state: item.sponsor_state,
+	}
+	let source = $('#bill_result_template').html();
+	let template = Handlebars.compile(source);
+	let html = template(context);
+	return html
 }
 
 function renderTitle(item){
@@ -55,8 +60,8 @@ function renderTitle(item){
 
 function truncate(title){
 	// truncates obscenely long titles at the last space before the 125th char
-	let snip = title.slice(0,settings.titleLength);
-	let finalSpace = snip.lastIndexOf(" ");
+	const snip = title.slice(0,settings.titleLength);
+	const finalSpace = snip.lastIndexOf(" ");
 	return snip.slice(0,finalSpace) + "…";
 }
 
@@ -119,34 +124,33 @@ function displayBill(data){
 	} else {
 		summary = 'No summary available.'
 	}
-	if (bill.enacted){		
+	if (bill.enacted){
+		// Selects most salient date format
 		datefield = `Enacted: ${bill.enacted}`;
 	} else {
 		datefield = `Introduced: ${bill.introduced_date}`;
 	}
-	$('.detail-view').html(
-		`	<div class="back-to-results">
-				<a href="#" class="back-to-results-link">&lt; Back to results</a>
-			</div>
-			<div class="bill-title">
-				<h1>${bill.title}</h1>
-			</div>
-			<div class="date">
-				<p>${datefield}</p>
-			</div>
-			<div class="sponsor-tile">
-				<h2>Sponsor:</h2>
-				<p><a href="${bill.sponsor_uri}" class="rep-request">${bill.sponsor_title} ${bill.sponsor} (<span class="affil-${bill.sponsor_party}">${bill.sponsor_party}</span>-${bill.sponsor_state})</a></p>
-				<div class="donors">
-				</div>
-			</div>
-			<div class="bill-summary">
-				<h3>Summary:</h3>
-				<p>${summary}</p>
-			</div>
-		`	
-	);
+
+	// handlebars context dic and calls below
+	let context = {
+		title: bill.title,
+		date: datefield,
+		sponsor_uri: bill.sponsor_uri,
+		sponsor_title: bill.sponsor_title,
+		sponsor: bill.sponsor,
+		sponsor_part: bill.sponsor_party,
+		sponsor_state: bill.sponsor_state,
+		summary: summary,
+	}
+
+	let source = $('#bill_details_template').html();
+	let template = Handlebars.compile(source);
+	let html = template(context);
+
+	$('.detail-view').html(html);
+
 	handleReturnToResults();
+	// retrieves results info from cache
 }
 
 // legislator functionality
@@ -176,11 +180,6 @@ function getRepCIDFromPropublica(data){
 	let cid = data.results[0].crp_id;
 	console.log("CID is: " + cid);
 	return cid;
-}
-
-function getRepDataFromOpenSecrets(cid){
-	console.log('getRepDataFromOpenSecrets called');
-	console.log('this feature not yet implemented');
 }
 
 // core functionality
@@ -239,31 +238,10 @@ function displaySplash(){
 	$('.detail-view').html('');
 	$('nav').prop('hidden',true)
 	$('.splash-content').prop('hidden',false)
-	$('.splash-content').html(
-		`
-		<header role="banner">
-			<h1 class="maintitle">Follow the Money</h1>
-			<h2 class="subtitle">Trace the money behind legislation</h2>
-		</header>
 
-		<section class="primary search" role="search">
-			<form action="#" class="search-form">
-				<label for="search-type" class="search-type-label">I want to search for </label>
-				<select class="search-type" name="search-type" id="search-type" aria-label="Choose what to search for">
-					<option value="b" selected="selected">a bill</option>
-					<option value="l">a legislator</option>
-				</select>
-				<label for="search-query" class="js-search-icon"><i class="fa fa-search" aria-hidden="true"></i></label>
-				<input type="text" name="search-query" class="search-query" aria-label="search" placeholder="Search for a bill...">
-				<button type="submit">Search</button>
-			</form>
-			<div class="instructions">
-				<p class="js-search-bill">Enter a topic or bill name to start tracing the finances behind legislation.</p>
-				<p class="js-search-rep" hidden>Search for a legislator to find their major donors, legislation they've sponsored, and their voting record.</p>
-			</div>
-		</section>
-		`
-	);
+	let splashContent = $('#splash-content-template').html()
+
+	$('.splash-content').html(splashContent);
 }
 
 function hideSplash(){
@@ -274,21 +252,8 @@ function hideSplash(){
 
 function showTopBarSearch(){
 	$('nav').prop('hidden',false);
-	$('nav').html(
-		`
-		<h4 class="brand"><a href="#" onClick="displaySplash()">Follow the <i class="fa fa-money"></i></a></h4>
-		<div class="nav search" role="search">
-			<form action="#" class="nav search-form">
-				<select class="nav search-type" name="search-type" id="search-type" aria-label="Choose what to search for">
-					<option value="b" selected="selected" aria-label="search for a bill">Bill</option>
-					<option value="l" aria-label="search for a legislator">Legislator</option>
-				</select>				
-				<input type="text" name="nav search-query" class="search-query" aria-label="search" placeholder="Search for a bill...">
-				<button type="nav submit"><i class="fa fa-search" aria-hidden="true"></i></button>			
-			</form>			
-		</div>		
-		`
-	);
+	let topBarSearch = $('#top-nav-template').html();
+	$('nav').html(topBarSearch);
 }
 
 function clearContent(){
